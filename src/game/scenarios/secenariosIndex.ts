4198c0de76b8a709f6d722f03d889e4305c9bbc8
Roope <noreply@example.com>
2026-05-10 21:51:14 +0800

    Set up initial project structure for tragedy-cycle using Vite and boardgame.io

--- src/game/scenarios/secenariosIndex.ts
+++ src/game/scenarios/secenariosIndex.ts
@@ -1,15 +1,27 @@
+import { AHR_001 } from "./custom/Another_Horizon_Revised/AHR_001";
 import { BTX_001 } from "./custom/Basic_Tragedy_X/BTX_001";
 import { FS_001 } from "./custom/First_Steps/FS_001";
 import { FS_002 } from "./custom/First_Steps/FS_002";
 import { NEW_SCENARIO } from "./custom/First_Steps/NEW_SCENARIO";
 import { HS_01 } from "./custom/Haunted_Stage/HS_01";
+import { HSA_001 } from "./custom/Haunted_stage_Again/HSA_001";
+import { LL_001 } from "./custom/Last_Lair/LL_001";
+import { MC_001 } from "./custom/Mystery_Circle/MC_001";
+import { MZ_001 } from "./custom/Midnight_Zone/MZ_001";
+import { WM_001 } from "./custom/Weird_Mythology/WM_001";
 import { fs_001 } from "./fs/fs_001";
 
 export const scenariosIndex = {
-  fs_001: fs_001,
+  AHR_001: AHR_001,
+  BTX_001: BTX_001,
   FS_001: FS_001,
+  FS_002: FS_002,
   NEW_SCENARIO: NEW_SCENARIO,
   HS_01: HS_01,
-  FS_002: FS_002,
-  BTX_001: BTX_001,
+  HSA_001: HSA_001,
+  LL_001: LL_001,
+  MC_001: MC_001,
+  MZ_001: MZ_001,
+  WM_001: WM_001,
+  fs_001: fs_001,
 };
